fbb6e7d57582641bd3aa71855c58a943878acad9
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-01 07:23:18 +0100

    new

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -11,3 +11,4 @@ print(f"Addition of {number1} and {number2} is {addition}")
 print(f"Subtraction of {number1} and {number2} is {subtraction}")
 print(f"Multiplication of {number1} and {number2} is {multiplication}")
 
+#Just testing changes

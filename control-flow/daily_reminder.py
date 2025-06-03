ab08047a33cf52045fb02dfd6623d98b64a1c226
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-03 07:38:32 +0100

    daily reminder update

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,9 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+
+priority = priority.lower()
+
+match priority:
+    case "high"
+    

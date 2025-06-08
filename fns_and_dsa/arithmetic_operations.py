79852c8a20918cc01c63daba9daa94d157973de7
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-08 12:10:31 +0100

    task 0 completed

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    operation = operation.lower()
+    
+    if(operation == "add" or "+" ):
+        return num1 + num2
+    elif(operation == "subtract" or "-"):
+        return num1 - num2 
+    elif(operation == "multiply" or "*" or "x"):
+        return num1 * num2
+    elif(operation == "divide" or "/"):
+        if(num1 > 0):
+            return num1 / num2
+        return "The divided can't be 0, check the numbers and try again."
+    else:
+        return  "Invalid operator entered"      

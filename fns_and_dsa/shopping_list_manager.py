3fc82a494797a4e4a896d6e575550e818839b44e
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-08 16:38:26 +0100

    add and remove functionalities done

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,11 +12,24 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            # Prompt for and add an item
-            pass
+            item = input("Enter the item to add to shopping list: ")
+            shopping_list.append(item)
+            print(f" '{item}' has beed added.")
+            cont = input("do you wish to continue operations Y/N: ")
+            cont = cont.lower()
+            if cont == "n":
+                break
         elif choice == '2':
-            # Prompt for and remove an item
-            pass
+            item = input("Enter item to remove: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"'{item}' has been removed.")
+            else:
+                print(f"'{item}' not found in the list.")
+            cont = input("do you wish to continue operations Y/N: ")
+            cont = cont.lower()
+            if cont == "n":
+                break
         elif choice == '3':
             # Display the shopping list
             pass

5bbee150738e582a78433e00894eaa7b455501cf
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-08 16:56:12 +0100

    completed task 1

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,8 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter the item to add to shopping list: ")
+            item = input("Enter the item to add: ")
+            item = item.lower()
             shopping_list.append(item)
             print(f" '{item}' has beed added.")
             cont = input("do you wish to continue operations Y/N: ")
@@ -21,6 +22,7 @@ def main():
                 break
         elif choice == '2':
             item = input("Enter item to remove: ")
+            item = item.lower()
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"'{item}' has been removed.")
@@ -31,8 +33,16 @@ def main():
             if cont == "n":
                 break
         elif choice == '3':
-            # Display the shopping list
-            pass
+            if shopping_list == []:
+                print("Your shopping list is empty")
+            else: 
+                print("Your shopping List:")
+                for item in shopping_list:
+                    print("-" + item)
+            cont = input("do you wish to continue operations Y/N: ")
+            cont = cont.lower()
+            if cont == "n":
+                break
         elif choice == '4':
             print("Goodbye!")
             break

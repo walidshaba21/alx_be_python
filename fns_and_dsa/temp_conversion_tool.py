df689526f16eeffc70b2de0ee8f4cce841050386
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-09 11:48:43 +0100

    Task 3 completed

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,31 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celcius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
+    return celsius * CELCIUS_TO_FAHRENHEIT_FACTOR + 32
+
+def get_temperature():
+    while True:
+        temp_value = input("Enter the temperature to convert: ")
+        
+        if temp_value.isdigit():
+            temp_value = int(temp_value)
+            break
+        else:
+            print("Not a valid temperature value.")
+    temp_conversion = input("Is this temperature in Celcius or Farenheit? (C/F): ")
+    temp_conversion = temp_conversion.lower()
+    if temp_conversion == "c":
+        return convert_to_fahrenheit(temp_value)
+    elif temp_conversion == "f":
+        return convert_to_celcius(temp_value)
+
+
+print(get_temperature()) 
+                        
+    

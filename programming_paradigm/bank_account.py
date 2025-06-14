458ec2d3a2fd40191755342b1fc3f15f18723cff
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-14 06:46:22 +0100

    task1 done with bugs

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,5 +3,18 @@ class BankAccount:
         self.account_balance = initial_balance
 
         def deposit(self, amount):
-            return self.account_balance =  account_balance + initial_balance, print("Balance has been update")
+            self.account_balance = self.account_balance + self.amount
+            return f"{self.amount} has been deposited you new balance is {self.account_balance}"
+
+        def withdraw(self, amount):
+            if self.account_balance == 0:
+                return "Your account balance is Zero! deposit an amount to be able to make withdrwal."
+            elif self.amount > self.account_balance:
+                return "Insufficient amount in you bank account to make withdraw!"
+            else:
+                self.account_balance = self.account_balance - self.amount
+                return f"Your new account balance is {self.account_balance}, {self.amount} has been withdrawn."
+
+        def display_balance(self):
+            return f"Your account Balance is : {self.account_balance}."
 

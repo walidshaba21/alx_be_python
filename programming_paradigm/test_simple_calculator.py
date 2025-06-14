7fda67d7e924f4c0d284958b7cf98af32dda6731
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-14 11:02:51 +0100

    template changes

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,14 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        # Add more assertions to thoroughly test the add method.

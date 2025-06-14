8180f57d261a16dfeb5d1d8dc5ec25a580204d51
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-14 09:28:15 +0100

    two deecimals

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,3 @@
-# bank_account.py
-
 class BankAccount:
     def __init__(self, initial_balance=0):
         self.account_balance = initial_balance
@@ -15,4 +13,4 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")

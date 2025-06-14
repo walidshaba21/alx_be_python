45ae1b94df2c780f28dff5a927a09ba72e984b5b
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-14 06:26:22 +0100

    testing task 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,7 @@
+class BankAccount:
+    def __init__(self, initial_balance = 0):
+        self.account_balance = initial_balance
+
+        def deposit(self, amount):
+            return self.account_balance =  account_balance + initial_balance, print("Balance has been update")
+

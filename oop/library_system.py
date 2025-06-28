064ef17a7c2fdf06f3ba9d02fb265df23d136de8
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-28 13:37:24 +0100

    Mastering Inheriatance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self. title = title
         self.author = author
 
-     def __str__(self):
+    def __str__(self):
         return f"{self.title} by {self.author}"
 
 class EBook(Book):

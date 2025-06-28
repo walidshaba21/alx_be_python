35c23c3952adc4013339fb088b73765f37c35f9a
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-06-28 13:40:07 +0100

    Mastering Inheriatance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,29 +1,31 @@
+# Base Class - Book
 class Book:
     def __init__(self, title: str, author: str):
-        self. title = title
+        self.title = title
         self.author = author
 
-    def __str__(self):
-        return f"{self.title} by {self.author}"
+    def get_info(self):
+        return f"Book: {self.title} by {self.author}"
 
+# Derived Class - EBook
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def __str__(self):
-        return f"{super().__str__()} [EBook, {self.file_size}MB]"
+    def get_info(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
+# Derived Class - PrintBook
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def __str__(self):
-        return f"{super().__str__()} [Print, {self.page_count} pages]"
+    def get_info(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
-
-
+# Composition - Library class
 class Library:
     def __init__(self):
         self.books = []
@@ -32,7 +34,5 @@ class Library:
         self.books.append(book)
 
     def list_books(self):
-        if not self.books:
-            print("The library has no books yet.")
         for book in self.books:
-            print(book)
+            print(book.get_info())
